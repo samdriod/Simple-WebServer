264e36d493032bd770574ec9bc5658e00de37e82
Sam <skarki48@myseneca.ca>
2024-09-25 11:42:05 -0400

    Initial commit with .gitignore

---
+++ modules/projects.js
@@ -0,0 +1,39 @@
+const projectData = require("../data/projectData.json");
+const secotorData = require("../data/sectorData.json");
+
+let projects = [];
+
+function Initialize() {
+    projects = JSON.parse(JSON.stringify(projectData));
+
+    let secMap = new Map();
+    secotorData.forEach(sec => {
+        secMap.set(sec.id, sec.sector_name);
+    });
+
+    projects.forEach(proj => {
+        if (secMap.has(proj.sector_id)) {
+            proj.sector = secMap.get(proj.sector_id);
+        }
+    });
+
+    return Promise.resolve();
+}
+
+function getAllProjects() {
+    return Promise.resolve(projects);
+}
+
+function getProjectById(projectId) {
+    const matchedProj = projects.find(proj => proj.id === projectId);
+    return matchedProj ? Promise.resolve(matchedProj) : Promise.reject(new Error("Unable to find requested project"));
+}
+
+function getProjectsBySector(sectorStr) {
+    const matchedSec = projects.filter(proj =>
+        proj.sector.toLowerCase().includes(sectorStr.toLowerCase())
+        );
+    return matchedSec ? Promise.resolve(matchedSec) : Promise.reject(new Error("Unable to find requested project"));
+}
+
+module.exports = {Initialize, getAllProjects, getProjectById, getProjectsBySector};
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const projectData = require("./modules/projects");
+
+projectData.Initialize();
+
+console
+
+let app = express();
+let HTTP_PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+    res.send("Assignment 2: Samip Kari - Student Id");
+});
+
+app.get("/solutions/projects", async (req, res) => {
+    res.send(await projectData.getAllProjects());
+});
+
+app.get("/solutions/projects/id-demo", async (req, res) => {
+    try {
+        res.send(await projectData.getProjectById(100));
+    } catch (error) {
+        res.send(error.message);
+    }
+});
+
+app.get("/solutions/projects/sector-demo", async (req, res) => {
+    try {
+        res.send(await projectData.getProjectsBySector("Agriculture"));
+    } catch (error) {
+        res.send(error.message);
+    }
+});
+
+
+app.listen(HTTP_PORT, () => {
+    console.log(`hello from ${HTTP_PORT}`)
+});
+
