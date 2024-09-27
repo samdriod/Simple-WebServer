230baa74cbb4d08928d63c3adaa9d386eb1d5227
Sam <skarki48@myseneca.ca>
2024-09-27 14:11:09 -0400

    Added header for submission

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* BTI325 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Samip Karki Student      ID: 141867234      Date: 2024/09/27
+*
+********************************************************************************/
+
 const express = require("express");
 const projectData = require("./modules/projects");
 
