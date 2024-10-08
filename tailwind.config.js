39600d44108614237924d8ad63af760a9e78a5c1
Sam <skarki48@myseneca.ca>
2024-10-07 20:21:59 -0400

    Update package.json and tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+const plugin = require("@tailwindcss/typography");
+const { default: daisyui } = require("daisyui");
+
+module.exports = {
+    content: [`./public/views/**/*.html`],
+    plugin: [require('@tailwindcss/typography'), require('daisyui')],
+};
