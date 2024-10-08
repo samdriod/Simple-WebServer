48c8f2c54b17a0c67da5a6acad52a44be9791849
Sam <skarki48@myseneca.ca>
2024-10-07 20:30:18 -0400

    Update tailwind.config.js to add a new theme for daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,7 @@ const { default: daisyui } = require("daisyui");
 module.exports = {
     content: [`./public/views/**/*.html`],
     plugin: [require('@tailwindcss/typography'), require('daisyui')],
+    daisyui: {
+        themes: ["business"]
+    }
 };
