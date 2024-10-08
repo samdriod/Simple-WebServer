0d6d007391ed9326edf56943ebc6f6077dbb0dbb
Sam <skarki48@myseneca.ca>
2024-10-07 21:57:46 -0400

    Update package.json and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ const { default: daisyui } = require("daisyui");
 
 module.exports = {
     content: [`./public/views/**/*.html`],
-    plugin: [require('@tailwindcss/typography'), require('daisyui')],
+    plugins: [require('@tailwindcss/typography'), require('daisyui')],
     daisyui: {
-        themes: ["business"]
+        themes: ["garden", "forest"]
     }
 };
