c6373fe9c6087d5db114e2e2e21c8f01d7e108dd
Sam <skarki48@myseneca.ca>
2024-10-11 14:20:13 -0400

    Refactor card layout in about.html and home.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,3 @@
-const plugin = require("@tailwindcss/typography");
-const { default: daisyui } = require("daisyui");
-const themes = require("daisyui/src/theming/themes");
-
 module.exports = {
   content: [`./public/views/**/*.html`],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
@@ -18,13 +14,13 @@ module.exports = {
           // "primary-content": "#000000",
           "primary-content": "#fff",
 
-          //   secondary: "#8E4162",
-          //   accent: "#5c7f67",
+          // secondary: "#8E4162",
+          // accent: "#5c7f67",
           secondary: "#1DB88E",
           accent: "#1DB8AB",
 
-          //   neutral: "#19362D",
-          //   "base-100": "#171212",
+          // neutral: "#19362D",
+          // "base-100": "#171212",
           neutral: "#291E00",
           "base-100": "#e9e7e7",
         },
