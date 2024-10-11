6daea1b6dcb656ff66fb98401a40bf26e209f640
Sam <skarki48@myseneca.ca>
2024-10-11 12:30:01 -0400

    Update tailwind.config.js to add custom theme for daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
 const plugin = require("@tailwindcss/typography");
 const { default: daisyui } = require("daisyui");
+const themes = require("daisyui/src/theming/themes");
 
 module.exports = {
-    content: [`./public/views/**/*.html`],
-    plugins: [require('@tailwindcss/typography'), require('daisyui')],
-    daisyui: {
-        themes: ["garden", "forest"]
-    }
+  content: [`./public/views/**/*.html`],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        myTheme: {
+          primary: "#1eb854",
+          "color-scheme": "light",
+          "neutral-content": "#e9e7e7",
+          "base-content": "#100f0f",
+          "--rounded-btn": "1.9rem",
+
+          // "primary-content": "#000000",
+          "primary-content": "#fff",
+
+          //   secondary: "#8E4162",
+          //   accent: "#5c7f67",
+          secondary: "#1DB88E",
+          accent: "#1DB8AB",
+
+          //   neutral: "#19362D",
+          //   "base-100": "#171212",
+          neutral: "#291E00",
+          "base-100": "#e9e7e7",
+        },
+      },
+    ],
+  },
 };
