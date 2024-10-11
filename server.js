9e932fa03c5994677ca403a2125eec3d99bae538
Sam <skarki48@myseneca.ca>
2024-10-11 15:24:58 -0400

    Refactor card layout in 404.html and home.html

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 /*********************************************************************************
-* BTI325 – Assignment 3
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
-* No part of this assignment has been copied manually or electronically from any other source
-* (including web sites) or distributed to other students.
-*
-* Name: Samip Karki Student      ID: 141867234      Date: 2024/09/27
-* Published URL: https://simple-web-server-one.vercel.app/
-********************************************************************************/
+ * BTI325 – Assignment 3
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Name: Samip Karki Student      ID: 141867234      Date: 2024/09/27
+ * Published URL: https://simple-web-server-one.vercel.app/
+ ********************************************************************************/
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const projectData = require("./modules/projects");
 
 projectData.Initialize();
@@ -17,38 +17,43 @@ let app = express();
 let HTTP_PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 
+// app.use((req, res, next) => {
+//   console.log(`Request from: ${req.get("user-agent")} [${new Date()}]`);
+//   next();
+// });
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/views/home.html"));
+  res.sendFile(path.join(__dirname, "/public/views/home.html"));
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/views/about.html"));
+  res.sendFile(path.join(__dirname, "/public/views/about.html"));
 });
 
 app.get("/solutions/projects", async (req, res) => {
-    res.send(await projectData.getAllProjects());
+  res.send(await projectData.getAllProjects());
 });
 
 app.get("/solutions/projects/id-demo", async (req, res) => {
-    try {
-        res.send(await projectData.getProjectById(1));
-        // res.send(await projectData.getProjectById(100)); // invalid id
-    } catch (error) {
-        res.status(404).send(error.message);
-    }
+  try {
+    res.send(await projectData.getProjectById(1));
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
 app.get("/solutions/projects/sector-demo", async (req, res) => {
-    try {
-        res.send(await projectData.getProjectsBySector("Agriculture"));
-        // res.send(await projectData.getProjectsBySector("Game")); // invalid sector
-    } catch (error) {
-        res.status(404).send(error.message);
-    }
+  try {
+    res.send(await projectData.getProjectsBySector("Agriculture"));
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "public/views/404.html"));
+});
 
 app.listen(HTTP_PORT, () => {
-    console.log(`hello from ${HTTP_PORT}`)
+  console.log(`hello from ${HTTP_PORT}`);
 });
-
