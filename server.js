f73852945947ebc29f264ac2c8f4ca3814b32dfd
Sam <skarki48@myseneca.ca>
2024-11-01 18:28:46 -0400

    Refactor server initialization and enhance error handling with improved message formatting

--- server.js
+++ server.js
@@ -4,15 +4,13 @@
  * No part of this assignment has been copied manually or electronically from any other source
  * (including web sites) or distributed to other students.
  *
- * Name: Samip Karki Student      ID: 141867234      Date: 2024/09/27
+ * Name: Samip Karki Student      ID: 141867234      Date: 2024/19/01
  * Published URL: https://climatesolutions.vercel.app/
  * URL maybe subject to change please pull from github and check the README for latest URL
  ********************************************************************************/
 const express = require("express");
 const path = require("path");
 const projectData = require("./modules/projects");
-
-projectData.Initialize();
 
 let app = express();
 let HTTP_PORT = process.env.PORT || 3000;
@@ -32,7 +30,7 @@ app.get("/about", (req, res) => {
 app.get("/solutions/projects", async (req, res) => {
   try {
     let data;
-    if (Object.keys(req.query).length ===0) {
+    if (Object.keys(req.query).length === 0) {
       data = await projectData.getAllProjects();
     } else if (req.query.sector === "Land Sinks") {
       data = await projectData.getProjectsBySector("Land Sinks");
@@ -48,23 +46,37 @@ app.get("/solutions/projects", async (req, res) => {
     });
   } catch (error) {
     console.log(error.message);
-    res.status(404).render(path.join(__dirname, "public/views/404.ejs"), {message: `No projects found for sector: ${req.query.sector}.`});
+    res.status(404).render(path.join(__dirname, "public/views/404.ejs"), {
+      message: `No projects found for sector: ${req.query.sector}.`,
+    });
   }
 });
 
 app.get("/solutions/projects/:id", async (req, res) => {
   try {
     let data = await projectData.getProjectById(parseInt(req.params.id));
-    res.render(path.join(__dirname, "/public/views/project.ejs"), { project: data});
+    res.render(path.join(__dirname, "/public/views/project.ejs"), {
+      project: data,
+    });
   } catch (error) {
-    res.status(404).render(path.join(__dirname, "public/views/404.ejs"), {message: "Unable to find requested project."});
+    res.status(404).render(path.join(__dirname, "public/views/404.ejs"), {
+      message: "Unable to find requested project.",
+    });
   }
 });
 
 app.use((req, res, next) => {
-  res.render(path.join(__dirname, "public/views/404.ejs"), {message : "I'm sorry, we're unable to find what you're looking for."});
+  res.render(path.join(__dirname, "public/views/404.ejs"), {
+    message: "I'm sorry, we're unable to find what you're looking for.",
+  });
 });
 
-app.listen(HTTP_PORT, () => {
-  console.log(`Listening on ${HTTP_PORT}`);
-});
+projectData
+  .Initialize()
+  .then((message) => {
+    console.log("Data loaded");
+    app.listen(HTTP_PORT, () => {
+      console.log(`Listening on ${HTTP_PORT}`);
+    });
+  })
+  .catch((error) => console.error(`Data not loaded, Error: ${error.message}`));
