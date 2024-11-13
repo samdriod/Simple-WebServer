4c518bc3d4e8547b2164116964d8c804649eb9de
Sam <skarki48@myseneca.ca>
2024-11-12 22:47:17 -0500

    Refactor server.js to encapsulate database connection and initialization logic within an async main function

--- server.js
+++ server.js
@@ -17,94 +17,102 @@ require("pg");
 
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.PGDATABASE,
-  process.env.PGUSER,
-  process.env.PGPASSWORD,
-  {
-    host: process.env.PGHOST,
-    dialect: "postgres",
-    port: 5432,
-    dialectOptions: {
-      ssl: { rejectUnauthorized: false },
-    },
-  }
-);
+async function main() {
+  await projectData.Initialize();
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.log("Unable to connect to the database:", err);
-  });
-
-let app = express();
-let HTTP_PORT = process.env.PORT || 3000;
-
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/public/views"));
-
-app.get("/", (req, res) => {
-  res.render(path.join(__dirname, "/public/views/home.ejs"));
-});
-
-app.get("/about", (req, res) => {
-  res.render(path.join(__dirname, "/public/views/about.ejs"));
-});
-
-app.get("/solutions/projects", async (req, res) => {
-  try {
-    let data;
-    if (Object.keys(req.query).length === 0) {
-      data = await projectData.getAllProjects();
-    } else if (req.query.sector === "Land Sinks") {
-      data = await projectData.getProjectsBySector("Land Sinks");
-    } else if (req.query.sector === "Industry") {
-      data = await projectData.getProjectsBySector("Industry");
-    } else if (req.query.sector === "Transportation") {
-      data = await projectData.getProjectsBySector("Transportation");
-    } else {
-      throw new Error("Invalid sector");
+  const sequelize = new Sequelize(
+    process.env.PGDATABASE,
+    process.env.PGUSER,
+    process.env.PGPASSWORD,
+    {
+      host: process.env.PGHOST,
+      dialect: "postgres",
+      port: 5432,
+      dialectOptions: {
+        ssl: { rejectUnauthorized: false },
+      },
     }
-    res.render(path.join(__dirname, "/public/views/projects.ejs"), {
-      projects: data,
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(404).render(path.join(__dirname, "public/views/404.ejs"), {
-      message: `No projects found for sector: ${req.query.sector}.`,
-    });
-  }
-});
+  );
 
-app.get("/solutions/projects/:id", async (req, res) => {
-  try {
-    let data = await projectData.getProjectById(parseInt(req.params.id));
-    res.render(path.join(__dirname, "/public/views/project.ejs"), {
-      project: data,
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Connection has been established successfully.");
+    })
+    .catch((err) => {
+      console.log("Unable to connect to the database:", err);
     });
-  } catch (error) {
-    res.status(404).render(path.join(__dirname, "public/views/404.ejs"), {
-      message: "Unable to find requested project.",
-    });
-  }
-});
 
-app.use((req, res, next) => {
-  res.render(path.join(__dirname, "public/views/404.ejs"), {
-    message: "I'm sorry, we're unable to find what you're looking for.",
+  let app = express();
+  let HTTP_PORT = process.env.PORT || 3000;
+
+  app.use(express.static("public"));
+  app.set("view engine", "ejs");
+  app.set("views", path.join(__dirname, "/public/views"));
+
+  app.get("/", (req, res) => {
+    res.render(path.join(__dirname, "/public/views/home.ejs"));
   });
-});
 
-projectData
-  .Initialize()
-  .then((message) => {
-    console.log("Data loaded");
-    app.listen(HTTP_PORT, () => {
-      console.log(`Listening on ${HTTP_PORT}`);
+  app.get("/about", (req, res) => {
+    res.render(path.join(__dirname, "/public/views/about.ejs"));
+  });
+
+  app.get("/solutions/projects", async (req, res) => {
+    try {
+      let data;
+      if (Object.keys(req.query).length === 0) {
+        data = await projectData.getAllProjects();
+      } else if (req.query.sector === "Land Sinks") {
+        data = await projectData.getProjectsBySector("Land Sinks");
+      } else if (req.query.sector === "Industry") {
+        data = await projectData.getProjectsBySector("Industry");
+      } else if (req.query.sector === "Transportation") {
+        data = await projectData.getProjectsBySector("Transportation");
+      } else if (req.query.sector === "Electricity") {
+        data = await projectData.getProjectsBySector("Electricity");
+      } else if (req.query.sector === "Food, Agriculture, and Land Use") {
+        data = await projectData.getProjectsBySector("Food, Agriculture, and Land Use");
+      } else {
+        throw new Error("Invalid sector");
+      }
+      res.render(path.join(__dirname, "/public/views/projects.ejs"), {
+        projects: data,
+      });
+    } catch (error) {
+      console.log(error.message);
+      res.status(404).render(path.join(__dirname, "public/views/404.ejs"), {
+        message: `No projects found for sector: ${req.query.sector}.`,
+      });
+    }
+  });
+
+  app.get("/solutions/projects/:id", async (req, res) => {
+    try {
+      let data = await projectData.getProjectById(parseInt(req.params.id));
+      res.render(path.join(__dirname, "/public/views/project.ejs"), {
+        project: data,
+      });
+    } catch (error) {
+      res.status(404).render(path.join(__dirname, "public/views/404.ejs"), {
+        message: "Unable to find requested project.",
+      });
+    }
+  });
+
+  app.use((req, res, next) => {
+    res.render(path.join(__dirname, "public/views/404.ejs"), {
+      message: "So sorry, we're unable to find what you're looking for.",
     });
-  })
-  .catch((error) => console.error(`Data not loaded, Error: ${error.message}`));
+  });
+
+  app.listen(HTTP_PORT, () => {
+    console.log(`Listening on ${HTTP_PORT}`);
+  });
+}
+
+try {
+  main().then(() => console.log("Main Execution Successful."));
+} catch (error) {
+  console.error(`Encountered error: ${error.message}`);
+}
